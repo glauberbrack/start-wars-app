f98bbddbaceda354d64eb0164e5551e2555c02ae
Glauber Brack <glauber@brack.com.br>
2021-03-25 17:15:56 -0300

    feat: tests configurations

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -1,14 +1,16 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import { CategoryCard } from '../src/components';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const tree = renderer
+    .create(
+      <CategoryCard icon='arrow-right' name='Glauber Brack' color='#151515' screen='Glauber' />
+    )
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
