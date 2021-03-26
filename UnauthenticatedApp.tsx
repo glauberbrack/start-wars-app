017793afa32254809fcdba50d758f2badf8a5d3e
Glauber Brack <glauber@brack.com.br>
2021-03-26 10:51:11 -0300

    feat: create startship and films underconstruction page

--- UnauthenticatedApp.tsx
+++ UnauthenticatedApp.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import { createStackNavigator } from '@react-navigation/stack';
 import { Characters } from '~/screens/Characters';
+import { Films } from '~/screens/Films';
 import { Home } from '~/screens/Home';
 import { Planets } from '~/screens/Planets';
-
-// import { theme } from 'src/theme';
+import { Starships } from '~/screens/Starships';
 
 const Stack = createStackNavigator();
 
@@ -14,6 +14,8 @@ const UnauthenticatedApp: React.FC = () => (
     <Stack.Screen name='Home' component={Home} />
     <Stack.Screen name='Planets' component={Planets} />
     <Stack.Screen name='Characters' component={Characters} />
+    <Stack.Screen name='Starships' component={Starships} />
+    <Stack.Screen name='Films' component={Films} />
   </Stack.Navigator>
 );
 
---
+++ src/screens/Starships/index.tsx
@@ -0,0 +1,1 @@
+export { default as Starships } from './Starships';
